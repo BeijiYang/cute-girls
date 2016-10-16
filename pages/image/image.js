5087017d9f6c8898bd6601359a8de9d0d2b10cd6
BeijiYang <langyuyang@outlook.com>
2016-10-17 00:56:13 +0800

    add saveImage

--- pages/image/image.js
+++ pages/image/image.js
@@ -1,5 +1,7 @@
 Page({
   data: {
+    modalHidden: true,
+    toastHidden:true,
     // url: "https://avatars0.githubusercontent.com/u/16985427?v=3&s=40",
     url: "",
     who: "1024"
@@ -9,9 +11,46 @@ Page({
 
     this.setData({
       url: cb.url,
-      who: cb.who
+      who: cb.who,
     })
+  },
 
-    
+  onlongtap: function() {
+    this.setData({
+      modalHidden: false
+    })
+  },
+
+  save: function() {
+   var that = this;
+   downloadImage(that);
+   this.setData({
+      modalHidden: true
+    })
+  },
+
+  cancel: function() {
+    this.setData({
+      modalHidden: true
+    })
+  },
+  toastChange: function() {
+    this.setData({
+      toastHidden: true
+    })
   }
 })
+
+function downloadImage(that) {
+   wx.downloadFile({
+   url: that.data.url,
+   type: 'image',
+   success: function(res) {
+     console.log("download success")
+     that.setData({
+      toastHidden: false
+    })
+    }
+  })
+} 
+
