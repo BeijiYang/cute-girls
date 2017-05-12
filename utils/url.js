17f269d27ac4d6d4f6210ff4c4bc5ae5101eeb55
BeijiYang <langyuyang@outlook.com>
2017-05-12 15:23:17 +0800

    fixed

--- utils/url.js
+++ utils/url.js
@@ -1,6 +1,6 @@
-var baseUrl = "http://gank.io/api";
-var cuteGirlsUrl = baseUrl.concat("/data/福利/10/");
-var randomGirlUrl = baseUrl.concat("/data/福利/1/");
+var baseUrl = "https://gank.io/api";
+var cuteGirlsUrl = baseUrl.concat("/data/%E7%A6%8F%E5%88%A9/10/");
+var randomGirlUrl = baseUrl.concat("/data/%E7%A6%8F%E5%88%A9/1/");
 
 module.exports = {
     baseUrl: baseUrl,
