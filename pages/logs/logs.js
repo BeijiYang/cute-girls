9d449d83d66ab301ff311fc7dc09028ac0d7cc91
BeijiYang <langyuyang@outlook.com>
2017-06-12 16:48:12 +0800

    add gif

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,14 +1,39 @@
 //logs.js
 var util = require('../../utils/util.js')
+var app = getApp()
 Page({
   data: {
     logs: [],
-    motto: "1024"
+    motto: "1024",
+    userInfo: {},
+    btnText: "扫码",
+  },
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  scanCode: function () {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+      }
+    })
   },
   onLoad: function () {
+    var that = this
+
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(function (log) {
         return util.formatTime(new Date(log))
+      })
+    }),
+     
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
       })
     })
   }
